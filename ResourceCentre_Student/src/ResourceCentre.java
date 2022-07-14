c9d1aefa3e44f547cb364b69c710887e17f37919
21015972 <21015972@10.174.115.114>
2022-07-14 15:11:59 +0800

    update returnChromebook() in ResourceCentre

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -30,7 +30,7 @@ public class ResourceCentre {
 				ResourceCentre.setHeader("ITEM TYPES");
 				System.out.println("1. Camcorder");
 				System.out.println("2. Chromebook");
-
+				
 				int itemType = Helper.readInt("Enter option to select item type > ");
 
 				if (itemType == 1) {
@@ -55,7 +55,7 @@ public class ResourceCentre {
 				ResourceCentre.setHeader("ITEM TYPES");
 				System.out.println("1. Camcorder");
 				System.out.println("2. Chromebook");
-
+				
 				int itemType = Helper.readInt("Enter option to select item type > ");
 
 				if (itemType == 1) {
@@ -74,7 +74,7 @@ public class ResourceCentre {
 				ResourceCentre.setHeader("ITEM TYPES");
 				System.out.println("1. Camcorder");
 				System.out.println("2. Chromebook");
-
+				
 				int itemType = Helper.readInt("Enter option to select item type > ");
 				if (itemType == 1) {
 					// Return camcorder
@@ -106,7 +106,7 @@ public class ResourceCentre {
 		Helper.line(80, "-");
 
 	}
-
+	
 	public static void setHeader(String header) {
 		Helper.line(80, "-");
 		System.out.println(header);
@@ -141,7 +141,7 @@ public class ResourceCentre {
 		ResourceCentre.setHeader("CAMCORDER LIST");
 		String output = String.format("%-10s %-30s %-10s %-10s %-20s\n", "ASSET TAG", "DESCRIPTION",
 				"AVAILABLE", "DUE DATE","OPTICAL ZOOM");
-		output += retrieveAllCamcorder(camcorderList);	
+		 output += retrieveAllCamcorder(camcorderList);	
 		System.out.println(output);
 	}
 
@@ -149,22 +149,14 @@ public class ResourceCentre {
 		String output = "";
 		// write your code here
 		for (int i = 0; i < chromebookList.size(); i++) {
-<<<<<<< HEAD
 			output += String.format("%-10s %-30s %-10s %-10s %-20s\n", chromebookList.get(i).getAssetTag(),
 					chromebookList.get(i).getDescription(), ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
 					chromebookList.get(i).getDueDate(), chromebookList.get(i).getOs());
-=======
-
-			output += String.format("%-10s %-30s %-10s %-10s %-20s\n", chromebookList.get(i).getAssetTag(),
-					chromebookList.get(i).getDescription(), 
-					ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
-					chromebookList.get(i).getDueDate(),chromebookList.get(i).getOs());
->>>>>>> branch 'master' of https://github.com/21015431-Muhammad-Hakim/ResourceCentre_Student.git
 		}
 		return output;
 	}
 	public static void viewAllChromebook(ArrayList<Chromebook> chromebookList) {
-
+		
 		String output = retrieveAllChromebook(chromebookList);
 		System.out.println(output);
 	}
@@ -177,40 +169,40 @@ public class ResourceCentre {
 
 		Camcorder cc= new Camcorder(tag, description, zoom);
 		return cc;
-
+		
 	}
 	public static void addCamcorder(ArrayList<Camcorder> camcorderList, Camcorder cc) {
-
+		
 		camcorderList.add(cc);
-
+		
 	}
-
+	
 	public static Chromebook inputChromebook() {	
 		Chromebook cb =null;
 		// write your code here
 
 		return cb;
-
+		
 	}	
 	public static void addChromebook(ArrayList<Chromebook> chromebookList, Chromebook cb) {
 		// write your code here
 		chromebookList.add(cb);
 	}
-
+	
 	//================================= Option 3 Loan an item (CRUD - Update) =================================
 	public static boolean doLoanCamcorder(ArrayList<Camcorder> camcorderList, String tag, String dueDate) {
-
+		
 		boolean isLoaned = false;
 
 		for (int i = 0; i < camcorderList.size(); i++) {
 			if (tag.equalsIgnoreCase(camcorderList.get(i).getAssetTag())
 					&& camcorderList.get(i).getIsAvailable() == true) {
-
+				
 				camcorderList.get(i).setIsAvailable(false);
 				camcorderList.get(i).setDueDate(dueDate);
-
+				
 				isLoaned = true;
-
+				
 			}
 		}
 		return isLoaned;
@@ -226,7 +218,7 @@ public class ResourceCentre {
 			System.out.println("Camcorder " + tag + " loaned out");
 		}
 	}
-
+	
 	public static boolean doLoanChromebook(ArrayList<Chromebook> chromebookList, String tag, String dueDate) {
 		// write your code here
 		boolean isLoaned = false;
@@ -234,10 +226,10 @@ public class ResourceCentre {
 		for (int i = 0; i < chromebookList.size(); i++) {
 			if (tag.equalsIgnoreCase(chromebookList.get(i).getAssetTag())
 					&& chromebookList.get(i).getIsAvailable() == true) {
-
+				
 				chromebookList.get(i).setIsAvailable(false);
 				chromebookList.get(i).setDueDate(dueDate);
-
+				
 				isLoaned = true;				
 			}
 		}
@@ -255,7 +247,7 @@ public class ResourceCentre {
 			System.out.println("Camcorder " + tag + " loaned out");
 		}	
 	}
-
+	
 	//================================= Option 4 Return an item (CRUD - Update)=================================
 	public static boolean doReturnCamcorder(ArrayList<Camcorder> camcorderList,String tag) {
 		boolean isReturned = false;
@@ -266,17 +258,17 @@ public class ResourceCentre {
 				camcorderList.get(i).setIsAvailable(true);
 				camcorderList.get(i).setDueDate("");
 				isReturned = true;
-
+				
 			}
 		}
 		return isReturned;
-
+		
 	}
 	public static void returnCamcorder(ArrayList<Camcorder> camcorderList) {
 		ResourceCentre.viewAllCamcorder(camcorderList);
 		String tag = Helper.readString("Enter asset tag > ");
 		Boolean isReturned = doReturnCamcorder(camcorderList, tag);
-
+		
 		if (isReturned == false) {
 			System.out.println("Invalid asset tag");
 		} else {
@@ -299,7 +291,6 @@ public class ResourceCentre {
 	}
 	public static void returnChromebook(ArrayList<Chromebook> chromebookList) {
 		// write your code here
-<<<<<<< HEAD
 		ResourceCentre.viewAllChromebook(chromebookList);
 		String tag = Helper.readString("Enter asset tag > ");
 		Boolean isReturned = doReturnChromebook(chromebookList, tag);
@@ -307,12 +298,9 @@ public class ResourceCentre {
 		if (isReturned == false) {
 			System.out.println("Invalid asset tag");
 		} else {
-			System.out.println("Camcorder " + tag + " returned");
+			System.out.println("Chromebook " + tag + " returned");
 		}
 	
-=======
-
->>>>>>> branch 'master' of https://github.com/21015431-Muhammad-Hakim/ResourceCentre_Student.git
 	}
 
 
